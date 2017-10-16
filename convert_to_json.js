b16f1c00c63571d0781c9eb591c66daffa1aa0ca
xuh <hayley.xu@york.ca>
2017-10-16 10:16:26 -0400

    add trim() to account for weird spaces that user entered incorrectly:

--- convert_to_json.js
+++ convert_to_json.js
@@ -2,9 +2,9 @@ convertExcel = require('excel-as-json').processFile;
 options = {sheet:'1', isColOriented: false, omitEmtpyFields: false}
 
 
-excel_file = 'C:\\Users\\xuh\\Desktop\\ori_excel_to_json\\excel_to_json_nodejs\\excel_file\\uplaod_domain_name.xlsx'
-json_file = 'C:\\Users\\xuh\\Desktop\\ori_excel_to_json\\excel_to_json_nodejs\\json_file\\upload_domain_name.json'
-json_file2 = 'C:\\Users\\xuh\\Desktop\\ori_excel_to_json\\excel_to_json_nodejs\\json_file\\upload_domain_name3.json'
+excel_file = 'C:\\Users\\xuh\\Desktop\\excel_to_json_nodejs\\excel_file\\uplaod_domain_name.xlsx'
+json_file = 'C:\\Users\\xuh\\Desktop\\excel_to_json_nodejs\\json_file\\upload_domain_name.json'
+//json_file2 = 'C:\\Users\\xuh\\Desktop\\ori_excel_to_json\\excel_to_json_nodejs\\json_file\\upload_domain_name.json'
 
 reg = /\(([0-9)]+)\)/
 
@@ -30,14 +30,17 @@ convertExcel(excel_file, json_file, options, function (err, data) {
                           output = {}
                           
                           for (i in dict){
-                            if ((dict[i]['CLASSIFICATION'] != "") && (!dict[i]['CLASSIFICATION'].includes('Attribute heading') ) ){
+                            if ((dict[i]['CLASSIFICATION'].trim() != "") && (!dict[i]['CLASSIFICATION'].includes('Attribute heading') ) ){
                               //TODO: save the temp_dict to output dict 
                               //for(item in temp_dict) {
                               //  output[item] = temp_dict[item];
                               //}
-
-                              key = dict[i]['CLASSIFICATION'] + '/' + dict[i]['SUBCLASSIFICATION']
-
+                              if (data[i]['SUBCLASSIFICATION'].trim() != ""){
+                              	key = dict[i]['CLASSIFICATION'].replace(reg, "").trim() + '/' + dict[i]['SUBCLASSIFICATION'].replace(reg, "").trim()
+                              }
+                              else{
+                              	key = dict[i]['CLASSIFICATION'].replace(reg, "").trim()
+                              }
                               temp_dict = {}
 
                               for (attr in data[i]['ATTRIBUTE']){
@@ -46,10 +49,12 @@ convertExcel(excel_file, json_file, options, function (err, data) {
                                   temp_dict[attr] = {};
                                   //TODO: calculate the number
                                   if (data[i]['SUBCLASSIFICATION'].trim() != ""){
-                                    temp_dict[attr]['NAME'] = data[i]['CLASSIFICATION'].replace(reg, "").trim() + '/' + data[i]['SUBCLASSIFICATION'].replace(reg, "").trim() + ' - ' + data[i]['ATTRIBUTE'][attr].replace(reg, "").trim()
+                                    //temp_dict[attr]['NAME'] = data[i]['CLASSIFICATION'].replace(reg, "").trim() + '/' + data[i]['SUBCLASSIFICATION'].replace(reg, "").trim() + ' - ' + data[i]['ATTRIBUTE'][attr].replace(reg, "").trim()
+                                    temp_dict[attr]['NAME'] = data[i]['ATTRIBUTE'][attr].replace(reg, "").trim()
+                                    
                                     }
                                   else{
-                                    temp_dict[attr]['NAME'] = data[i]['CLASSIFICATION'].replace(reg, "").trim() + ' - ' + data[i]['ATTRIBUTE'][attr].replace(reg, "").trim()
+                                    temp_dict[attr]['NAME'] = data[i]['ATTRIBUTE'][attr].replace(reg, "").trim()
                                     }
                                   }
                               }
@@ -92,7 +97,7 @@ convertExcel(excel_file, json_file, options, function (err, data) {
                           const fs = require('fs');
                           const content = JSON.stringify(output);
 
-                          fs.writeFile(json_file2, content, 'utf8', function (err) {
+                          fs.writeFile(json_file, content, 'utf8', function (err) {
                               if (err) {
                                   return console.log(err);
                               }
